9d58f3833259fc382598de361308bf58066b796a
z251257144 <zmsoul@126.com>
2015-06-15 16:14:40 +0800

    去除iOS7waring

--- MBALib/SDK/Foundation_Categories/NSDate/NSDate+Helper.m
+++ MBALib/SDK/Foundation_Categories/NSDate/NSDate+Helper.m
@@ -104,7 +104,7 @@
 	NSDateComponents *components2 = [CURRENT_CALENDAR components:DATE_COMPONENTS fromDate:aDate];
 	
 	// Must be same week. 12/31 and 1/1 will both be week "1" if they are in the same week
-	if (components1.week != components2.week) return NO;
+	if (components1.weekOfYear != components2.weekOfYear) return NO;
 	
 	// Must have a time interval under 1 week. Thanks @aclark
 	return (fabs([self timeIntervalSinceDate:aDate]) < D_WEEK);
@@ -315,7 +315,7 @@
 - (NSInteger) week
 {
 	NSDateComponents *components = [CURRENT_CALENDAR components:DATE_COMPONENTS fromDate:self];
-	return components.week;
+	return components.weekOfYear;
 }
 
 - (NSInteger) weekday
